59953ec312114249fd693562aefe8220cec4a1ce
SamuelArnaizdelaRosa <samuel.arnaiz@campusfp.es>
2023-05-15 11:42:10 +0200

    Estilos register con campos validados

--- StockServer/src/controllers/stock.controller.js
+++ StockServer/src/controllers/stock.controller.js
@@ -16,45 +16,6 @@ const config = {
     }
 }
 
-/*var connection = async () => {
-    try {
-        await mysql.connect(config)
-        console.log('Conexión exitosa a la base de datos SQL Server!')
-    } catch (err) {
-        console.error('Error al intentar conectarse a la base de datos SQL Server:', err.message)
-    }
-}
-
-connection.call();*/
-
-
-
-//var Connection = require('tedious').Connection;
-
-/*var config = {
-    server: 'localhost',  
-    authentication: {
-        type: 'default',
-        options: {
-            userName: 'sa', //update me
-            password: 'araDST2010'  //update me
-        }
-    },
-
-    options: {
-        // If you are on Microsoft Azure, you need encryption:
-        encrypt:false,
-        database: 'DSTNET/Central', 
-        port:1433
-    }
-}
-
-var connection = new Connection(config);
-connection.on('connect', function (err) {
-    console.log(err);
-})
-
-connection.connect();*/
 class GestorStock {
     constructor() { }
 
